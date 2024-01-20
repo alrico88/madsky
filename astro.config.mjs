9547ef2f707718a1053cdc56dccd5bfe3818006c
Alberto Rico <alrico88@gmail.com>
2024-01-20 17:00:50 +0100

    perf: added compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 import node from "@astrojs/node";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [mdx(), solidJs()],
+  integrations: [mdx(), solidJs(), compress()],
   vite: {
     optimizeDeps: {
       exclude: ["sharp"],
