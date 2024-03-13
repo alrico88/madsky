1fd684957214e2789b0e5bb2a4fb6fa89860a93d
Alberto Rico <alrico88@gmail.com>
2024-03-13 20:57:28 +0100

    fix: pass process.env

--- src/helpers/db.ts
+++ src/helpers/db.ts
@@ -1,6 +1,6 @@
 import * as schema from '@root/schema';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-const queryClient = postgres(import.meta.env.DATABASE_URL);
+const queryClient = postgres(process.env.DATABASE_URL as string);
 
 export default drizzle(queryClient, { schema });
